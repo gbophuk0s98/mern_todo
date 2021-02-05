fc67c764c0bd6d75745f1680f3938c6f208a3da2
gbophuk0s98 <pasha.surta@mail.ru>
2021-02-05 22:28:58 +0300

    server upd

--- server.js
+++ server.js
@@ -5,15 +5,14 @@ const app = express()
 const PORT = 8080
 
 app.use(express.json({ extended: true }))
+
+app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+})
+
 app.use('/api/auth', require('./routes/auth.routes'))
-
-if (process.env.NODE_ENV == 'production'){
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
 
 async function start(){
     try
